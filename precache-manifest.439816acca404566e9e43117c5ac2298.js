743b9b789d3e2b9eb06e3a79b09ea561d4fba9d1
tubacatal <tubacatal>
2020-04-27 00:14:51 +0300

    Updates

---
+++ precache-manifest.439816acca404566e9e43117c5ac2298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787f391b3bb3c5ff95f360726d1ab644",
+    "url": "/tubacatal.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "706cd82cdfe9aa99ea7c",
+    "url": "/tubacatal.github.io/monsters-rolodex/static/css/main.19a8969c.chunk.css"
+  },
+  {
+    "revision": "a736ad8f1db94dde207e",
+    "url": "/tubacatal.github.io/monsters-rolodex/static/js/2.6ace61d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tubacatal.github.io/monsters-rolodex/static/js/2.6ace61d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706cd82cdfe9aa99ea7c",
+    "url": "/tubacatal.github.io/monsters-rolodex/static/js/main.0aa0c7db.chunk.js"
+  },
+  {
+    "revision": "3dbd78a662c533a35424",
+    "url": "/tubacatal.github.io/monsters-rolodex/static/js/runtime-main.9657541c.js"
+  }
+]);
